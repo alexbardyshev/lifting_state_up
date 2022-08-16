e9a08cb42f72acb22ba7460f6613c9eeb6a1e2f3
Oleksìi <bardyshev@gmail.com>
2022-08-16 21:12:58 +0300

    Initial commit

---
+++ lifting-state-up/src/App.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import {QuantitySelector} from "./components/QuantitySelector";
+
+class App extends React.Component {
+  state = {
+    studentsCount: 800,
+    employeesCount: 30,
+  }
+
+  addStudent = () => {
+    this.setState(state => ({
+      studentsCount: state.studentsCount + 1,
+    }));
+  };
+
+  removeStudent = () => {
+    this.setState(state => ({
+      studentsCount: state.studentsCount - 1,
+    }));
+  };
+
+  addEmployee = () => {
+    this.setState(state => ({
+      employeesCount: state.employeesCount + 1,
+    }));
+  };
+
+  removeEmployee = () => {
+    this.setState(state => ({
+      employeesCount: state.employeesCount - 1,
+    }));
+  };
+
+  render() {
+    const {
+      studentsCount,
+        employeesCount,
+    } = this.state;
+
+    return (
+        <div className="App">
+          <h1>
+            Mates count:
+            {studentsCount + employeesCount}
+          </h1>
+
+          <QuantitySelector
+            title="Students"
+            quantity={studentsCount}
+            onRemove={this.removeStudent}
+            onAdd={this.addStudent}
+          />
+
+          <QuantitySelector
+              title="Employees"
+              quantity={employeesCount}
+              onRemove={this.removeEmployee}
+              onAdd={this.addEmployee}
+          />
+        </div>
+    )
+  }
+}
+
+export default App;
